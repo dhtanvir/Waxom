b3796bce62175f6e7649fe291c7bd0235b72b0d1
MD Tanvirul Islam <dhtanvir1323@gmail.com>
2022-02-06 18:53:23 +0600

    full responsive
    update

---
+++ Waxom.js
@@ -0,0 +1,87 @@
+// /*========bxSlider=============== */
+//  $(document).ready(function () {
+//      $('.slider').bxSlider();
+//  });
+// /*========bxSlider=============== */
+
+
+
+/*===============================*/
+// $('.responsive').slick({
+//     dots: true,
+//     infinite: false,
+//     speed: 300,
+//     slidesToShow: 4,
+//     slidesToScroll: 4,
+//     responsive: [{
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 3,
+//                 infinite: true,
+//                 dots: true
+//             }
+//         },
+//         {
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 2
+//             }
+//         },
+//         {
+//             breakpoint: 480,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1
+//             }
+//         }
+//         // You can unslick at a given breakpoint now by adding:
+//         // settings: "unslick"
+//         // instead of a settings object
+//     ]
+// });
+
+
+
+/*====================================*/ 
+
+
+$(function () {
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: true,
+        centerMode: true,
+        centerPadding: '0',
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
